dfed3fade10b300a11b50c9eeeb1a02cd9f18fde
Daya Chettri <Dayasagar073@gmail.com>
2023-06-10 01:34:03 +0530

    installed chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+const el = document.getElementById('root') as HTMLDivElement;
+const root = createRoot(el);
+root.render(
+  <ChakraProvider>
     <App />
-  </React.StrictMode>,
-)
+  </ChakraProvider>
+);
