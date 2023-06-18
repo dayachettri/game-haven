fbf681b06d5a5490b94f59c61e3c0525330c087b
Daya Chettri <Dayasagar073@gmail.com>
2023-06-19 00:35:02 +0530

    refactor: remove duplicate interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -11,7 +11,7 @@ import { BsGlobe } from 'react-icons/bs';
 import { SiNintendo } from 'react-icons/si';
 import { HStack, Icon } from '@chakra-ui/react';
 import { IconType } from 'react-icons';
-import { Platform } from '../hooks/useGames';
+import { Platform } from '../hooks/usePlatforms';
 
 interface Props {
   platforms: Platform[];
