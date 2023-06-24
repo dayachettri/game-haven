0fc5992f9ab5598b52cda77576fca8db518f784f
Daya Chettri <Dayasagar073@gmail.com>
2023-06-25 03:48:12 +0530

    refactor: update logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,12 @@
 import { HStack, Image } from '@chakra-ui/react';
-import logo from '../assets/logo.webp';
+import logo from '../assets/logo1.jpg';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
 
 function NavBar() {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Image src={logo} boxSize="50px" borderRadius={10} objectFit="cover" />
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
