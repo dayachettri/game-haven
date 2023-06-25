462f30bda167e610dce5dbf2eaf79e5020c780be
Daya Chettri <Dayasagar073@gmail.com>
2023-06-26 05:24:55 +0530

    build a back button

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,8 +1,9 @@
-import { Heading, Spinner } from '@chakra-ui/react';
+import { Button, Heading, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import ExpandableText from '../components/ExpandableText';
 import GameAttributes from '../components/GameAttributes';
 import useGame from '../hooks/useGame';
+import { Link } from 'react-router-dom';
 
 function GameDetailPage() {
   const { slug } = useParams();
@@ -14,6 +15,11 @@ function GameDetailPage() {
 
   return (
     <>
+      <Button padding={0}>
+        <Link to="/" style={{ padding: '15px' }}>
+          Go Back
+        </Link>
+      </Button>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
